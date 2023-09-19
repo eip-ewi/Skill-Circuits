51ec6abe604f5e2154d4224590d4c3a13559c311
Bernadett Bakos <B.A.Bakos-1@student.tudelft.nl>
2023-09-19 12:37:21 +0200

    Version repository

---
+++ src/main/java/nl/tudelft/skills/repository/UserVersionRepository.java
@@ -0,0 +1,37 @@
+/*
+ * Skill Circuits
+ * Copyright (C) 2022 - Delft University of Technology
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package nl.tudelft.skills.repository;
+
+import java.util.Optional;
+import java.util.Set;
+
+import nl.tudelft.skills.model.UserVersion;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
+
+public interface UserVersionRepository extends JpaRepository<UserVersion, Long> {
+	default UserVersion findByIdOrThrow(Long id) {
+		return findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("UserVersion was not found: " + id));
+	}
+
+	Set<UserVersion> getByPersonId(Long id);
+
+	Optional<UserVersion> findByPersonId(Long id);
+}
