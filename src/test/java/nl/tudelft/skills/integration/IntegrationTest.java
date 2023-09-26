d7ccbb76a116a8ebbdba52de10e2547ae7400d5f
Fanni Fiedrich <F.Z.Fiedrich@student.tudelft.nl>
2023-09-26 19:28:22 +0200

    Add tests for expanded skills (complete task, open overlay)

--- src/test/java/nl/tudelft/skills/integration/IntegrationTest.java
+++ src/test/java/nl/tudelft/skills/integration/IntegrationTest.java
@@ -40,7 +40,7 @@ public abstract class IntegrationTest {
 	@BeforeAll
 	static void launchBrowser() {
 		playwright = Playwright.create();
-		browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setTimeout(1000));
+		browser = playwright.chromium().launch();
 	}
 
 	@BeforeAll
@@ -57,7 +57,7 @@ public abstract class IntegrationTest {
 	@BeforeEach
 	void createContextAndPage() {
 		context = browser.newContext();
-		page = context.newPage();
+		page = context.newPage(); // TODO set default timeout
 	}
 
 	@AfterEach
@@ -129,35 +129,38 @@ public abstract class IntegrationTest {
 
 	/**
 	 * Performs the actions to publish an edition. This method assumes that the correct page (homepage) is
-	 * open, and that the user is a teacher for the corresponding course.
+	 * open, and that the user is not logged in.
 	 *
 	 * @param courseCode The code of the course to publish an edition in.
 	 * @param edition    The name of the edition to publish.
 	 */
 	protected void publishEdition(String courseCode, String edition) {
+		logInAs("cseteacher1", "cseteacher1", "CSE Teacher 1");
 		navigateToEditionSetup(courseCode, edition);
 		// Wait until timeout or publish button is visible
 		Locator publish = page.getByRole(AriaRole.BUTTON,
 				new Page.GetByRoleOptions().setName("Publish edition"));
 		publish.waitFor();
 		publish.click();
+		logOutAs("CSE Teacher 1");
 	}
 
 	/**
-	 * Performs the actions to un-publish an edition. This method assumes that the correct page (homepage) is
-	 * open, that the user is a teacher for the corresponding course, and that the edition was published
-	 * previously.
+	 * Performs the actions to un-publish an edition.This method assumes that the correct page (homepage) is
+	 * open, that the user is not logged in and that the edition was published previously.
 	 *
 	 * @param courseCode The code of the course to un-publish an edition in.
 	 * @param edition    The name of the edition to un-publish.
 	 */
 	protected void unpublishEdition(String courseCode, String edition) {
+		logInAs("cseteacher1", "cseteacher1", "CSE Teacher 1");
 		navigateToEditionSetup(courseCode, edition);
 		// Wait until timeout or un-publish button is visible
 		Locator publish = page.getByRole(AriaRole.BUTTON,
 				new Page.GetByRoleOptions().setName("Unpublish edition"));
 		publish.waitFor();
 		publish.click();
+		logOutAs("CSE Teacher 1");
 	}
 
 }
