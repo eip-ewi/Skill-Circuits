31bc47cda21dbf2b23efc454e8a3b224f460a658
Fanni Fiedrich <F.Z.Fiedrich@student.tudelft.nl>
2024-06-14 14:21:03 +0200

    Create function for setting overlay state and add tests

--- src/test/ts/main.test.ts
+++ src/test/ts/main.test.ts
@@ -1,7 +1,7 @@
 // @ts-ignore
-import {toggleOverlay} from "../../main/ts/main.ts";
+import {toggleOverlay, setOverlayState} from "../../main/ts/main.ts";
 
-test("Test overlay", () => {
+test("Test toggle overlay", () => {
     const dialog = document.createElement("dialog");
     dialog.showModal = function () { this.open = true; }
     dialog.close = function () { this.open = false; }
@@ -13,5 +13,36 @@ test("Test overlay", () => {
     toggleOverlay("overlay");
     expect(dialog.open).toBe(true);
     toggleOverlay("overlay");
+    expect(dialog.open).toBe(false);
+});
+
+test("Test set overlay to opened", () => {
+    const dialog = document.createElement("dialog");
+    dialog.showModal = function () { this.open = true; }
+    dialog.close = function () { this.open = false; }
+    dialog.id = "overlay";
+
+    document.body.append(dialog)
+
+    expect(dialog.open).toBe(false);
+    setOverlayState("overlay", true);
+    expect(dialog.open).toBe(true);
+    setOverlayState("overlay", true);
+    expect(dialog.open).toBe(true);
+});
+
+test("Test set overlay to closed", () => {
+    const dialog = document.createElement("dialog");
+    dialog.showModal = function () { this.open = true; }
+    dialog.close = function () { this.open = false; }
+    dialog.open = true;
+    dialog.id = "overlay";
+
+    document.body.append(dialog)
+
+    expect(dialog.open).toBe(true);
+    setOverlayState("overlay", false);
+    expect(dialog.open).toBe(false);
+    setOverlayState("overlay", false);
     expect(dialog.open).toBe(false);
 });
