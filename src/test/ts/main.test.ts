38960f782b066ca6921646d82a3e4cb95f139cf0
Fanni Fiedrich <F.Z.Fiedrich@student.tudelft.nl>
2024-06-15 14:47:59 +0200

    Fixup for dialog tests

--- src/test/ts/main.test.ts
+++ src/test/ts/main.test.ts
@@ -1,48 +1,50 @@
 // @ts-ignore
 import {toggleOverlay, setOverlayState} from "../../main/ts/main.ts";
 
-test("Test toggle overlay", () => {
-    const dialog = document.createElement("dialog");
+const dialog: HTMLDialogElement = document.createElement("dialog");
+
+beforeAll(() => {
     dialog.showModal = function () { this.open = true; }
     dialog.close = function () { this.open = false; }
     dialog.id = "overlay";
-
     document.body.append(dialog)
+});
 
+test("Test toggle overlay", () => {
+    dialog.open = false;
     expect(dialog.open).toBe(false);
+
+    // False to true
     toggleOverlay("overlay");
     expect(dialog.open).toBe(true);
+
+    // True to false
     toggleOverlay("overlay");
     expect(dialog.open).toBe(false);
 });
 
 test("Test set overlay to opened", () => {
-    const dialog = document.createElement("dialog");
-    dialog.showModal = function () { this.open = true; }
-    dialog.close = function () { this.open = false; }
-    dialog.id = "overlay";
-
-    document.body.append(dialog)
-
+    dialog.open = false;
     expect(dialog.open).toBe(false);
+
+    // False to true
     setOverlayState("overlay", true);
     expect(dialog.open).toBe(true);
+
+    // True to true
     setOverlayState("overlay", true);
     expect(dialog.open).toBe(true);
 });
 
 test("Test set overlay to closed", () => {
-    const dialog = document.createElement("dialog");
-    dialog.showModal = function () { this.open = true; }
-    dialog.close = function () { this.open = false; }
     dialog.open = true;
-    dialog.id = "overlay";
-
-    document.body.append(dialog)
-
     expect(dialog.open).toBe(true);
+
+    // True to false
     setOverlayState("overlay", false);
     expect(dialog.open).toBe(false);
+
+    // False to false
     setOverlayState("overlay", false);
     expect(dialog.open).toBe(false);
 });
