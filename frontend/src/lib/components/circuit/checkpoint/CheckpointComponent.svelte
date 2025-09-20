83247788b8ce27cc9dd9883eb35241f5ac1d4766
Fanni Fiedrich <F.Z.Fiedrich@student.tudelft.nl>
2025-09-20 16:27:42 +0200

    Make checkpoint warning a pop up

--- frontend/src/lib/components/circuit/checkpoint/CheckpointComponent.svelte
+++ frontend/src/lib/components/circuit/checkpoint/CheckpointComponent.svelte
@@ -19,6 +19,28 @@
 
     let row: number = $derived(Math.max(...skills.map(skill => skill.row!)));
 
+    let openWarnDialog: boolean = $state(false);
+    let element: HTMLDialogElement | undefined = $state();
+
+    $effect(() => {
+        if (element === undefined) {
+            return;
+        }
+        if (openWarnDialog) {
+            element.showModal();
+        }
+    });
+
+    function showWarnDialog() {
+        openWarnDialog = true;
+    }
+
+    function checkForClose(event: MouseEvent) {
+        if (event.target === element) {
+            openWarnDialog = false;
+        }
+    }
+
 </script>
 
 <div class="checkpoint" style:grid-row={row + 1} data-completed={completed} data-focused={focused}>
@@ -28,13 +50,27 @@
             <span class="deadline">{moment(checkpoint.deadline).format("D MMMM YYYY HH:mm")}</span>
         </div>
         {#if warn}
-            <div class="warning">
+            <button class="warning" onclick={showWarnDialog}>
                 <span class="fa-solid fa-triangle-exclamation"></span>
-                <p>
-                    This checkpoint has passed. Some of the tasks in the skills before this point might not be completable anymore.
-                    Missing a checkpoint is no cause for concern, but if you are very far behind, we advice you talk to a teaching assistant, academic counselor, or lecturer.
-                </p>
-            </div>
+                This checkpoint has passed.
+            </button>
+            {#if openWarnDialog}
+                <dialog bind:this={element} onclick={checkForClose} class="dialog">
+                    <span class="fa-solid fa-triangle-exclamation"></span>
+                    <div class="dialog-header">
+                        <p>This checkpoint has passed.</p>
+                        <p>Some of the tasks in the skills before this point might not be completable anymore.</p>
+                    </div>
+                    <div class="dialog-content">
+                        Missing a checkpoint is no cause for concern, but if you are very far behind, we advise you talk to a teaching assistant, academic counsellor, or lecturer.
+                        You can find more information about how to reach the academic counsellors
+                        <a target="_blank" href="https://www.tudelft.nl/en/student/eemcs-student-portal/organisation/academic-counsellors">
+                            here
+                        </a>
+                        <span class="fa-solid fa-arrow-up-right-from-square"></span>.
+                    </div>
+                </dialog>
+            {/if}
         {/if}
     </div>
 </div>
@@ -109,6 +145,71 @@
         display: flex;
         gap: 1em;
         padding: .5em 1em;
+        cursor: pointer;
+        transition: transform ease-in-out 150ms;
+    }
+
+    .warning:hover {
+        transform: scale(1.05);
+    }
+
+    .dialog {
+        border-radius: var(--warning-banner-border-radius);
+        background-color: var(--warning-pop-up-color);
+        color: var(--on-warning-banner-colour);
+        position: fixed;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%, -50%);
+        max-width: 45rem;
+        padding: 1.1em;
+        border: none;
+        display: grid;
+        grid-template-columns: repeat(2, auto);
+        grid-template-rows: repeat(2, auto);
+        gap: 1em 1.6em;
+    }
+
+    .dialog::backdrop {
+        backdrop-filter: blur(.15rem);
+    }
+
+    .dialog a {
+        color: var(--on-warning-banner-colour);
+    }
+
+    .dialog > span:first-child {
+        font-size: 3em;
+        grid-row: 1;
+        grid-column: 1;
+        display: flex;
+        align-items: center;
+    }
+
+    .dialog-header {
+        grid-row: 1;
+        grid-column: 2;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+    }
+
+    .dialog-header > p:first-child {
+        font-size: 1.5em;
+        font-weight: bold;
+    }
+
+    .dialog-header > p:last-child {
+        font-size: 1em;
+    }
+
+    .dialog-content {
+        grid-row: 2;
+        grid-column: 2;
+    }
+
+    .dialog-content > span {
+        font-size: 0.6em;
     }
 
     .warning .fa-solid {
